436d7ea1285f6ae94e13882c0198ba6750288cf7
CHOI DAE GEON <chleorjs37@gmail.com>
2022-08-22 21:38:41 +0900

    라이프 사이클 작성

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Vue composition api 공부
+
+## setup
+
+> 컴퍼넌트가 생성되기 전에 진입하는 지점
+
+## Lifecycle hooks
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <h1>
-    {{ compTextLength }}
-  </h1>
-  <div>
-    <input type="text" v-model="text" />
-  </div>
   <router-view></router-view>
 </template>
 
@@ -12,57 +6,9 @@
 import { ref } from "@vue/reactivity";
 import { computed } from "vue";
 
-const useCount = () => {
-  const cnt = ref(0);
-
-  const compCnt = computed(() => {
-    return `${cnt.value}회`;
-  });
-
-  const increase = () => {
-    cnt.value++;
-  };
-
-  const decrease = () => {
-    cnt.value--;
-  };
-
-  return {
-    increase,
-    decrease,
-    compCnt,
-    cnt,
-  };
-};
-
-const useInput = () => {
-  const text = ref("");
-
-  const compTextLength = computed(() => {
-    return text.value.length;
-  });
-
-  return {
-    text,
-    compTextLength,
-  };
-};
-
 export default {
   name: "App",
-  setup() {
-    const { cnt, increase, decrease, compCnt } = useCount();
-    const { text, compTextLength } = useInput();
-
-    return {
-      compCnt,
-      increase,
-      decrease,
-      cnt,
-      compTextLength,
-      text,
-    };
-  },
+  setup() {},
 };
 </script>
 
